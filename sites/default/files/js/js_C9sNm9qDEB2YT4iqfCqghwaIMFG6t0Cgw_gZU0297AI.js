d322207385892addecd87f5efb1ff8ee332bb79d
zhoujie <j2963652956@gmail.com>
2025-12-31 19:40:10 +0800

    feat: theme up.

--- sites/default/files/js/js_C9sNm9qDEB2YT4iqfCqghwaIMFG6t0Cgw_gZU0297AI.js
+++
@@ -1,2 +0,0 @@
-/* @license GPL-2.0-or-later https://www.drupal.org/licensing/faq */
-(function($,Drupal){Drupal.behaviors.layoutParagraphsComponentForm={attach:function attach(context){$('[name="layout_paragraphs[layout]"]').on('change',(e)=>{$('.lpb-btn--save').prop('disabled',e.currentTarget.disabled);});$('.lpb-btn--save').prop('disabled',false);}};})(jQuery,Drupal);;
